bfd288006eb0f95616ec3aed749e01496d6e1f8e
Francesco Deleo <francescodeleo@Mac.localdomain>
2026-06-26 15:04:32 -0400

    Remove _headers passthrough from eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addPassthroughCopy("_headers");
   eleventyConfig.addPassthroughCopy("src/styles.css");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
 
